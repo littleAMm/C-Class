61e6c0714991e89331f15385139fc8492537d693
嘉兴 赵 <2634439734@qq.com>
2022-02-21 23:30:38 +0800

    第一次课堂作业

---
+++ 第一次课堂作业/第一次课堂作业/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 第一次课堂作业
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("你好！我是赵嘉兴");
+        }
+    }
+}
